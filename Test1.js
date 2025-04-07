e19f35b538f612cec5fe5338f379ab1cdce6fee3
Hydra777777 <164726019+Hydra777777@users.noreply.github.com>
2025-04-08 08:57:23 +1000

    Add files via upload

---
+++ Test1.js
@@ -0,0 +1,17 @@
+{
+  "airlineName": "USAF 33rd Wing Mustangs F-35B Lightning II (cadets)",
+  "liveries": [
+    {
+      "aircraft": "F-35B Lightning II (by JAaMDG)",
+      "name": "01",
+      "textureUrl": "https://i.ibb.co/gLg194QM/1.png",
+      "thumbnailUrl": "https://i.ibb.co/HpRC19X4/Screenshot-2025-04-07-225722.png"
+    },
+    {
+      "aircraft": "F-35B Lightning II (by JAaMDG)",
+      "name": "02",
+      "textureUrl": "https://i.ibb.co/4gjkcPnN/2.png"
+      "thumbnailUrl": "https://i.ibb.co/HpRC19X4/Screenshot-2025-04-07-225722.png"
+    }
+  ]
+}
